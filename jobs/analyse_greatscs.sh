42cfb5eb07ccd1fb47efc79ebc908f7ba54204cf
mathias157 <mathiasros@proton.me>
2026-06-30 13:51:38 +0200

    Syncing results from scenarios

--- jobs/analyse_greatscs.sh
+++ jobs/analyse_greatscs.sh
@@ -1,1 +1,7 @@
-pixi run analyse production --filters 'Scenario in ["EVN_INV" , "HPN_INV" , "TPN_INV" , "ELN_INV" , "HSNSSN_INV" , "HSN_INV" , "SSN_INV" , "EIN_INV" , "DCN_INV" , "H2N_INV" , "base_INV"] and Commodity =="ELECTRICITY" and Year == "2050"' --normalise
+extra_arg=$1
+
+for scenario in "EVN" "HPN" "TPN" "ELN" "HSNSSN" "HSN" "SSN" "EIN" "DCN" "H2N" "base"; do
+    pixi run sync-mainresults "$scenario"
+done
+
+pixi run analyse $extra_arg --filters 'Scenario in ["EVN_R2050" , "HPN_R2050" , "TPN_R2050" , "ELN_R2050" , "HSNSSN_R2050" , "HSN_R2050" , "SSN_R2050" , "EIN_R2050" , "DCN_R2050" , "H2N_R2050" , "base_R2050"] and Commodity =="ELECTRICITY" and Year == "2050"' production
