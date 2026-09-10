a63bbbc014deb880d3130af3df620daad8abcebb
mathias157 <mathiasros@proton.me>
2026-09-10 13:10:03 +0200

    Updating ALLN (previous commit fixed ELN scenario not activated for ALLN)

--- jobs/slurm/rolling_2050.sh
+++ jobs/slurm/rolling_2050.sh
@@ -5,7 +5,7 @@
 ### -- set the job Name --
 #SBATCH --job-name=GREAT_rolling_2050
 ### -- ask for number of cpus (default: 1) --
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=5
 ### -- specify that the cpus must be on the same node --
 #SBATCH --nodes=1
 ### -- set walltime limit: D-HH:MM:SS --
